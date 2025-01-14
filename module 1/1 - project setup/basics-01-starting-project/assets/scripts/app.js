879c17797020c792fa217c32adf92ba15b1ed24b
Rafael Marques Pincetta <rafaelmarquesp19@hotmail.com>
2025-01-13 22:25:25 -0300

    feat: strings

--- module 1/1 - project setup/basics-01-starting-project/assets/scripts/app.js
+++ module 1/1 - project setup/basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,12 @@
-alert('this works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+//template literal
+let calculationDescription = `( ${defaultResult}   + 10) 
+
+
+* 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
